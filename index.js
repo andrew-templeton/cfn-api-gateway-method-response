f6cb52e6308cbf77e1455e8ea4eaa6662161b0df
Andrew Templeton <andrw.tmplton@gmail.com>
2015-11-29 15:31:40 -0600

    v0.2.1; Change Upsert to handle resource conflict exceptions properly and do proper removal

--- index.js
+++ index.js
@@ -12,24 +12,43 @@ var Delete = CfnLambda.SDKAlias({
   ignoreErrorCodes: [404]
 });
 
-var Upsert = CfnLambda.SDKAlias({
-  api: APIG,
-  method: 'putMethodResponse',
-  downcase: true,
-  forceBools: ['ResponseParameters.*'],
-  returnPhysicalId: function(data, params) {
-    return [
-      params.RestApiId,
-      params.ResourceId,
-      params.HttpMethod,
-      params.StatusCode
-    ].join(':');
-  }
-});
+var Upsert = function(params, reply, existingId) {
+  CfnLambda.SDKAlias({
+    api: APIG,
+    method: 'putMethodResponse',
+    downcase: true,
+    forceBools: ['ResponseParameters.*'],
+    returnPhysicalId: function(data, params) {
+      return [
+        params.RestApiId,
+        params.ResourceId,
+        params.HttpMethod,
+        params.StatusCode
+      ].join(':');
+    }
+  })(params, function(createErr, createPhysical, createInfo) {
+    if (existingId && createErr &&
+      createErr.message === 'Response already exists for this resource') {
+      console.log('Method already exists, which is fine: %j', createErr);
+      return reply(null, existingId);
+    }
+    reply(createErr, createPhysical, createInfo);
+  });
+}
 
 exports.handler = CfnLambda({
   Create: Upsert,
-  Update: Upsert,
+  Update: function(physicalId, params, oldParams, reply) {
+    Delete(physicalId, oldParams, function(deleteErr) {
+      if (deleteErr) {
+        console.error('Could not delete old MethodResponse %s: %j', physicalId, deleteErr);
+        return reply('Could not delete old MethodResponse: ' +
+          (deleteErr.message || 'FATAL'));
+      }
+      console.log('Was able to delete old version of this resource. Proceeding with Upsert.');
+      Upsert(params, reply, physicalId);
+    });
+  },
   Delete: Delete,
   SchemaPath: [__dirname, 'schema.json']
 });
